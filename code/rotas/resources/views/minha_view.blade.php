dda2430617cd8c4e88419ab06393860a698f679b
Paulo Balzi <paulobalzi@c3po.balzi.local>
2019-01-06 01:45:20 -0200

    secao7_aula32

---
+++ code/rotas/resources/views/minha_view.blade.php
@@ -0,0 +1,7 @@
+<html>
+<head>Minha view</head>
+<body>
+	<h1> Minha View Hello!</h1>
+	<h2>{{$nome}}, {{$sobrenome}}</h2>
+</body>
+</html>
--- code/rotas/routes/web.php
+++ code/rotas/routes/web.php
@@ -16,3 +16,29 @@ Route::get('/nome/{id}', 'MeuControlador@getNomeById');
 
 // cria todas as rotas para o controller
 Route::resource('/cliente', 'ClientController');
+
+Route::get('minha-view', function() {
+    return view('minha_view')
+        ->with('nome', 'paulo')
+        ->with('sobrenome', 'sauro');
+});
+
+// Route::get('minha-view/{nome}/{sobrenome}', function($nome, $sobrenome) {
+//     return view('minha_view')
+//         ->with('nome',$nome)
+//         ->with('sobrenome', $sobrenome);
+// });
+Route::get('minha-view/{nome}/{sobrenome}', function($nome, $sobrenome) {
+    // $params = ['nome' => $nome, 'sobrenome' => $sobrenome];
+    // return view('minha_view', $params);
+
+    return view('minha_view', compact('nome', 'sobrenome'));
+});
+
+Route::get('/nao-existe', function() {
+    if (View::exists('nao-existe')) {
+        return view('nao-existe');
+    } else {
+        return view('error-404');
+    }
+});
