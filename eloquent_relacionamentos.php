09cb7c3bfff4f36b6efe6463b1ef1c0f01c8aac0
Paulo Balzi <paulobalzi@gmail.com>
2019-02-17 21:14:06 -0300

    belongsTo

--- eloquent_relacionamentos.php
+++ eloquent_relacionamentos.php
@@ -24,3 +24,52 @@ $c->endereco()->save($objEndereco)
 // poupa memória e recursosw
 
 $enderecos = Endereco::with(['cliente'])->get();
+
+// ---------------------------------------------------------------------------------------------------------------------
+// UM PARA MUITOS
+
+// Criando um seeder
+// > php artisan make:seeder CategoriaSeeder
+
+// file: database/seeds/CategoriaSeeder.php
+
+use Illuminate\Database\Seeder;
+class CategoriaSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('categorias')->insert(['nome' => 'Roupas']);.
+        DB::table('categorias')->insert(['nome' => 'Sapatos']);.
+        ...etc
+    }
+}
+
+// Depois de criado a sseder, necessário adicionar a classe de seeder no método
+// run da classe DatabaseSeeder
+// file: database/seeds/DatabaseSeeder.php
+use Illuminate\Database\Seeder;
+class DatabaseSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call(CategoriaSeeder::class);
+        $this->call(ProdutoSeeder::class);
+    }
+}
+
+// executar o seed
+// > php artisan db:seed
+
+// Relacionamento belongsTo
+// Exemplo:
+// Categoria(1) -> (N)Produtos
+// Produtos(1) -> (1)Categoria
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+class Produto extends Model
+{
+    function categoria()
+    {
+        return $this->belongsTo('App\Categoria');
+    }
+}
