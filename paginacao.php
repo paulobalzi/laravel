d86c5a3e2114518616021552287424212459e32c
Paulo Balzi <paulo.balzi@ifb.edu.br>
2019-03-20 19:07:36 -0300

    começando album de fotos

---
+++ paginacao.php
@@ -0,0 +1,8 @@
+<?php
+
+// resources/assets/sass/_variables.scss
+// configurações de tipografia
+
+// Tips
+// href="javascript:void(0);"
+// isso faz com que, no clique, o focu não vá para o topo da página
