948ac4474a198941a6549ca39572cb0809102cb6
Paulo Balzi <paulobalzi@gmail.com>
2019-04-10 22:34:34 -0300

    angular

---
+++ algum_fotos.php
@@ -0,0 +1,17 @@
+<?php
+
+// cria um link simbólico para uma pasta
+// php artisan storage:link
+
+// site com imagens para testes
+// lorenflickr.com
+
+// ----------------------------------
+// url: material.angular.io
+
+// necessário a instalação do pacote do npm
+// > sudo npm install -g @angular/cli
+
+// criar projeto angular dentro de asstes
+// cd /path/to/assets
+// > ng new <nome-projeto>
